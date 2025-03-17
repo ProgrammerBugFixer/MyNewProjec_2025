b1c120cac10fde2f3525d0d44a7ca7f7b589c9b0
ProgrammerBugFixer <prog_net@ukr.net>
2025-03-17 19:53:03 +0200

    sort select

--- MyNewProjec_2025/MyNewProjec_2025.cpp
+++ MyNewProjec_2025/MyNewProjec_2025.cpp
@@ -37,48 +37,48 @@ int main() {
 		cout  << array[i]<< ' ';
 	cout << endl;
 
-	//bool isSwap = false;
-	//for (int i = 0; i < ARRAY_SIZE; ++i) {
-	//	isSwap = false;
-	//	for (int j = 0; j < ARRAY_SIZE - 1 - i; ++j)
-	//		if (array[j] > array[j + 1]) {
-	//			//int temp = array[j];
-	//			//array[j] = array[j + 1];
-	//			//array[j + 1] = temp;
-	//			swap(array[j], array[j + 1]);
-	//			isSwap = true;
-	//		}
-	//	if (!isSwap)
-	//		break;
-	//}
-	//for (int i = 0; i < ARRAY_SIZE; ++i)
-	//	cout << array[i] << ' ';
-	//cout << endl;
-
-	bool isSort = false;
-	int counter = 0;
-	do {
-		cout << counter << " time -> ";
-		for (int i = 0; i < ARRAY_SIZE; ++i)
-			cout << array[i] << ' ';
-		cout << endl;
-		isSort = true;
-		for (int i = 0; i < ARRAY_SIZE - 1; ++i)
-			if (array[i] > array[i + 1]) {
-				isSort = false;
-				counter++;
-				for (int i = 0; i < ARRAY_SIZE; ++i)
-					swap(array[i], array[rand() % ARRAY_SIZE]);
-				break;
-			}
-	} while (!isSort);
-
-	cout << "new array -> " << endl;
+	bool isSwap = false;
 	for (int i = 0; i < ARRAY_SIZE; ++i) {
-		array[i] = rand() % 89 + 10;
-		cout << array[i] << ' ';
+		isSwap = false;
+		for (int j = 0; j < ARRAY_SIZE - 1 - i; ++j)
+			if (array[j] > array[j + 1]) {
+				//int temp = array[j];
+				//array[j] = array[j + 1];
+				//array[j + 1] = temp;
+				swap(array[j], array[j + 1]);
+				isSwap = true;
+			}
+		if (!isSwap)
+			break;
 	}
+	for (int i = 0; i < ARRAY_SIZE; ++i)
+		cout << array[i] << ' ';
 	cout << endl;
+
+	//bool isSort = false;
+	//int counter = 0;
+	//do {
+	//	cout << counter << " time -> ";
+	//	for (int i = 0; i < ARRAY_SIZE; ++i)
+	//		cout << array[i] << ' ';
+	//	cout << endl;
+	//	isSort = true;
+	//	for (int i = 0; i < ARRAY_SIZE - 1; ++i)
+	//		if (array[i] > array[i + 1]) {
+	//			isSort = false;
+	//			counter++;
+	//			for (int i = 0; i < ARRAY_SIZE; ++i)
+	//				swap(array[i], array[rand() % ARRAY_SIZE]);
+	//			break;
+	//		}
+	//} while (!isSort);
+
+	//cout << "new array -> " << endl;
+	//for (int i = 0; i < ARRAY_SIZE; ++i) {
+	//	array[i] = rand() % 89 + 10;
+	//	cout << array[i] << ' ';
+	//}
+	//cout << endl;
 
 
 	system("pause");
