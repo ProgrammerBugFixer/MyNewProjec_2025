ce6bcb96b8f23c70558d170bab23b84d073d2dc9
ProgrammerBugFixer <prog_net@ukr.net>
2025-03-21 20:14:43 +0200

    sort

--- MyNewProjec_2025/MyNewProjec_2025.cpp
+++ MyNewProjec_2025/MyNewProjec_2025.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <conio.h>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,73 +13,72 @@ using namespace std;
 int main() {
 	srand(time(NULL));
 
-
-	//int a = 10;
-	//int b = 100;
-
-	//cout << "A = " << a << " B = " << b << endl;
-
-	//a = a + b;//  10 + 100 =110
-	//b = a - b;//  110 - 100 = 10
-	//a = a - b;// 110 - 10 = 100
-
-	//int temp = a;
-	//a = b;
-	//b = temp;
-
-	//cout << "A = " << a << " B = " << b << endl;
-
-	const int ARRAY_SIZE = 7;
-	int array[ARRAY_SIZE];
-	for (int i = 0; i < ARRAY_SIZE; ++i)
-		array[i] = rand() % 90 + 10;
-
-	for (int i = 0; i < ARRAY_SIZE; ++i)
-		cout  << array[i]<< ' ';
-	cout << endl;
-
-	bool isSwap = false;
-	for (int i = 0; i < ARRAY_SIZE; ++i) {
-		isSwap = false;
-		for (int j = 0; j < ARRAY_SIZE - 1 - i; ++j)
-			if (array[j] > array[j + 1]) {
-				//int temp = array[j];
-				//array[j] = array[j + 1];
-				//array[j + 1] = temp;
-				swap(array[j], array[j + 1]);
-				isSwap = true;
-			}
-		if (!isSwap)
-			break;
-	}
-	for (int i = 0; i < ARRAY_SIZE; ++i)
-		cout << array[i] << ' ';
-	cout << endl;
-
-	//bool isSort = false;
-	//int counter = 0;
-	//do {
-	//	cout << counter << " time -> ";
-	//	for (int i = 0; i < ARRAY_SIZE; ++i)
-	//		cout << array[i] << ' ';
-	//	cout << endl;
-	//	isSort = true;
-	//	for (int i = 0; i < ARRAY_SIZE - 1; ++i)
-	//		if (array[i] > array[i + 1]) {
-	//			isSort = false;
-	//			counter++;
-	//			for (int i = 0; i < ARRAY_SIZE; ++i)
-	//				swap(array[i], array[rand() % ARRAY_SIZE]);
+	//int  array[5] = { 5,2,8,6,1 };
+	//int temp[5];
+	//temp[0] = array[0];
+	//for (int i = 1; i < 5; ++i) {//array
+	//	int j;
+	//	for(j=0; j<i; j++)//temp
+	//		if (array[i] < array[j]) {
+	//			int t = i;
+	//			while (t>j) {
+	//				temp[t] = temp[t - 1];
+	//				t--;
+	//			}
+	//			temp[j] = array[i];
 	//			break;
 	//		}
-	//} while (!isSort);
-
-	//cout << "new array -> " << endl;
-	//for (int i = 0; i < ARRAY_SIZE; ++i) {
-	//	array[i] = rand() % 89 + 10;
-	//	cout << array[i] << ' ';
+	//	if(j==i)
+	//		temp[j] = array[i];
 	//}
-	//cout << endl;
+	//int x;
+	//for (int i = 0; i < 5; ++i) {
+	//	x = array[i];
+	//	int j;
+	//	for (j = i - 1; j >= 0 && array[j] > x; j--)
+	//		array[j + 1] = array[j];
+	//	array[j + 1] = x;
+	//}
+
+
+	const int ARRAY_SIZE = 20;
+	int array[ARRAY_SIZE] = { 0 };
+	for (int i = 0; i < ARRAY_SIZE; ++i)
+		array[i] = rand() % 10 + 10;
+
+	for (int index : array)
+		cout << index << ' ';
+	cout << endl;
+
+	sort(array, array + ARRAY_SIZE);
+
+	for (int index : array)
+		cout << index << ' ';
+	cout << endl;
+
+	//binary sort
+	int start = 0;
+	int finish = ARRAY_SIZE - 1;
+	int searchNumber = 17;
+	int middleIndex;
+	while (start < finish) {
+		middleIndex = (start + finish) / 2;
+		if (array[middleIndex] == searchNumber) {
+			start = middleIndex;
+			break;
+		}
+		else if (array[middleIndex] < searchNumber)
+			start = middleIndex + 1;
+		else
+			finish = middleIndex - 1;
+	}
+
+	if (array[start] == searchNumber)
+		cout << "Search number position is " << start << endl;
+	else
+		cout << "Number is not found" << endl;
+
+	cout << *max_element(array, array+ARRAY_SIZE) << endl;
 
 
 	system("pause");
